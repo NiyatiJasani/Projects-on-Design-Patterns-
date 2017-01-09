11603b7f46dec0c8e6ae75ccf55a1b2de8161719
NiyatiJasani <niyatijasani6@gmail.com>
2017-01-09 10:21:10 +0530

    Update FootBall.java

--- Template pattern application/FootBall.java
+++ Template pattern application/FootBall.java
@@ -4,17 +4,17 @@ public class FootBall extends Game {
 
 	@Override
 	void initialize() {
-System.out.println("FootBall game initialized!!! start the game..");		
+System.out.println("FootBall game has intialized, start the game..");		
 	}
 
 	@Override
 	void startPlay() {
-		System.out.println("FootBall game Started!!! enjoy the game...");
+		System.out.println("FootBall game has Started!");
 	}
 
 	@Override
 	void endPlay() {
-		System.out.println("FootBall game Finished!!!!!!");
+		System.out.println("The game has Finished!");
 	}
 
 }
